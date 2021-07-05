389336afee82ce90e65cdaa72dd9e33674163ac4
Charles Agra <csagra216@gmail.com>
2021-07-05 11:09:06 -0300

    Initial commit

---
+++ cypress/integration/screenshot_google.js
@@ -0,0 +1,13 @@
+describe('screenshot', function () {
+    it('must take a screenshot of page', function () {
+        cy.visit('http://www.google.com')
+        
+        cy.screenshot('screenshot-google-current')
+
+        cy.fixture('../images/screenshot-google-original.png').then((original) => {
+            cy.fixture('../screenshots/screenshot_google.js/screenshot-google-current.png').then((current) => {
+                expect(current).to.equal(original)
+            })
+        })
+    })
+})
