38eac79cebd835d38f31c270f7ee8433bc4cf49e
Charles Agra <charles.agra@nodo.cc>
2021-08-18 17:43:10 -0300

    fix

--- cypress/integration/screenshot_google.js
+++ cypress/integration/screenshot_google.js
@@ -1,5 +1,13 @@
 describe('screenshot', function () {
     it('must take a screenshot of page', function () {
+
+        // levar isso para o frontend da embraer
+        // visitar o cms ou portal
+        // se precisar logar loga se não faz nada
+        // adicionar no projeto uma pasta com a imagem original ou pegar de um link -> s3 ou algo do tipo
+        // Talvez não seja necessário. Precisamos apenas do base64 da imagem e adicionamos isso na variavel diretamente(Esta forma eh melhor)
+        // tirar screenshot e retornar se o teste teve sucesso ou falha
+
         cy.visit('http://www.google.com')
         
         cy.screenshot('screenshot-google-current')
